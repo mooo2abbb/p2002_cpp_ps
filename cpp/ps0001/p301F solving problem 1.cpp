991e47e9b79f12cc912d04ef4d6f1bd264e4dfef
Salwa Sayed <salwasayed522@gmail.com>
2024-08-23 15:39:10 +0300

    Solving problem 1 (واجب الحساب)

---
+++ cpp/ps0001/p301F solving problem 1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1;
+    int num2;
+    int num3;
+
+
+cout<<"enter the first number /n1" << endl;
+cin>> num1;
+
+cout<<"enter the second number /n1" << endl;
+cin>>num2;
+
+
+cout<<"enter the third number /n1" << endl;
+cin>> num3;
+
+    cout << num1+num2+num3 << endl;
+    return 0;
+}
