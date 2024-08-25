49d1694a0e516214872b572ca3303373c5fcd050
eslam-gouhar <111485848+eslam-gouhar@users.noreply.github.com>
2024-08-26 01:16:30 +0300

    Add files via upload

---
+++ cpp/ps0001/ps0001soln.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+int main()
+{
+  int a;
+  int b;
+  int c;
+  cin >> a >> b >> c ;
+  int s = a + b + c ;
+  cout << s ;
+  return 0;
+}
