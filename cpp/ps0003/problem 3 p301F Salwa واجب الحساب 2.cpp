b1aeb9211b409c4f2d35d1fe343cae9df9e5fa89
Salwa Sayed <salwasayed522@gmail.com>
2024-09-05 19:43:49 +0300

    problem 3 واجب الحساب

---
+++ cpp/ps0003/problem 3 p301F Salwa واجب الحساب 2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+cout<<"--------------------------------------------" <<endl;
+cout<<"Problem 0003"<< endl;
+
+    int num;
+
+
+cout<<"enter the number" << endl;
+cin>> num;
+
+if (num % 2==0)
+{
+    cout <<"your number is even \n1";
+}
+else
+{
+    cout <<"your number is odd \n1";
+
+}
+
+
+
+
+    return 0;
+}
