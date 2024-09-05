aa50ea1ed68cf9cc887de6b8e91e4d148d4501c4
Salwa Sayed <salwasayed522@gmail.com>
2024-09-05 19:42:28 +0300

    Answers of Problem 2 مساعدة بسيطة

---
+++ cpp/ps0002/problem 2 p301F Salwa مساعدة بسيطة.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+//مساعدة بسيطة
+
+Short Int Num_residents = 30;
+
+Short Int medicine-alternative= 30;
+
+str Pr_members= {“Ahmed, Mohamed, Kareem, Ali”};
+
+float price = 25.5f;
+
+Char First_let= ‘A’;
+
+Long long int Num_babies = 1000,000,000;
+
+float result = 25.5f;
+
+double electricity = 1000,000,000;
+
+Long long int Age = 1000,000,000;
+
+    return 0;
+}
