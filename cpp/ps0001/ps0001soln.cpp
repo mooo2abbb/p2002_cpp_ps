bdcf469656022e56f7e972a84aff955da02c3a84
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-09-10 08:40:09 +0300

    problem 1 marked

--- cpp/ps0001/ps0001soln.cpp
+++ cpp/ps0001/ps0001soln.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
-#include <limits.h>
+
 using namespace std;
 
 int main()
 {
-  int a;
-  int b;
-  int c;
-  cin >> a >> b >> c ;
-  int s = a + b + c ;
-  cout << s ;
+  // prompting users for 3 numbers to add 1/2
+  short num1;
+  short num2;
+  short num3;
+  cin >> num1 >> num2 >> num3;
+
+  // outputting the result of the summation process 2/3
+  short sum = num1 + num2 + num3;
+  cout << sum;
   return 0;
 }
+// great work 😎 8/10 ⭐
