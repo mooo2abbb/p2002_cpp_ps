b3cd899d3bb6c4529bd39483a1951a4cf76c5ad4
eslam-gouhar <111485848+eslam-gouhar@users.noreply.github.com>
2024-09-10 22:22:13 +0300

    Add files via upload

---
+++ cpp/ps0002/ps0002soln.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+int main()
+{
+  short inhabitance = 20 ;
+  cout << inhabitance << endl;
+
+  short Alternatives = 3 ;
+  cout << Alternatives << endl;
+  
+  short team_members = 10 ;
+  cout << team_members << endl;
+
+  short bread_price = 5 ;
+  cout << bread_price << endl;
+  
+  char first_letter = 'E' ;
+  cout << first_letter << endl;
+
+  short Number_of_births = 1024061 ;
+  cout << Number_of_births << endl;
+
+  double dob = 17.f / 7.f ;
+  cout << dob << endl;
+
+  short Electricity_bill = 3948575 ;
+  cout << Electricity_bill << endl ;
+
+  short Age_in_seconds = 94608000 ;
+  cout << Age_in_seconds << endl ;
+
+  return 0;
+}
