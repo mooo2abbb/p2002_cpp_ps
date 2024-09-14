ffde3ed84542a8b532244656ccbe950a29b3a6e8
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-09-14 10:37:46 +0300

    problem 8 marked

--- cpp/ps0008/main.cpp
+++ cpp/ps0008/main.cpp
@@ -1,84 +1,99 @@
-#include <iostream>
-#include<limits>
+#include<iostream>
 #include<cctype>
 #include<string>
 
 using namespace std;
-void  decrypt(string msg,int key);
+
+void decrypt(string msg,int key);
 bool isnum(string s);
+
 int main()
-{ string key;
-string msg;
+{
+    // prompting user for valid ciphertext and key
+    string key;
+    string msg;
 
-cout <<"key: ";
-cin>>key;
- bool p=isnum(key);
- while (p==false){
-   cout <<"key: ";
-cin>>key;
-p=isnum(key);
-}
-int k=stoi(key);
- cin.ignore();
-cout<<"ciphertext: ";
-
-getline(cin,msg);
-
-
-decrypt(msg,k);
+    cout<<"key: ";
+    cin>>key;
+    
+    bool p=isnum(key);
+    while (p==false){
+        cout <<"key: ";
+        cin>>key;
+        p=isnum(key);
+    }
+    int k=stoi(key);
+    
+    cin.ignore();
+    cout<<"ciphertext: ";
+    getline(cin,msg); // Taking message 1/1
+    
+    // invoke your decrypt function to return the plaintext 1/1
+    decrypt(msg,k);
 
     return 0;
 }
+
+// Decrypting process 3/3
 void decrypt(string msg,int key){
-    string  plaintext;
+    string plaintext;
     key=key%26;
- for(char c:msg){
+    
+    for(char c:msg){
+    
+        if(isupper(c)){
+            c=c-key;
+            
+            if(c<'A'){
+                c+=26;
+            }
+            
+            else if(c>'Z'){
+                c-=26;
+            }
+            
+            plaintext+=c;
+            
+        }
+        
+        else if(islower(c)){
+            c=c-key;
+            
+            if(c<'a'){
+                c+=26;
+            }
+            
+            else if(c>'z'){
+                c-=26;
+            }
+            
+            plaintext+=c;
+        }
+        
+        
+        else {
+            plaintext+=c;
+        }
+        
+    } // DRY!
 
-  if(isupper(c)){
-   c=c-key;
-  if(c<'A'){
-    c+=26;
-  }else if(c>'Z'){
-  c-=26;}
-
-      plaintext+=c;
-
-  }
-
-    else if(islower(c)){
-    c=c-key;
-     if(c<'a'){
-    c+=26;
-  }else if(c>'z'){
-  c-=26;}
-
-  plaintext+=c;}
-
-
-    else{
-
-   plaintext+=c;}
-
- }
-
-cout<<"plaintext: "<<plaintext<<endl;
-
+    cout<<"plaintext: "<<plaintext<<endl;
+    
 }
 
+// checking key invalidity 3/3
 bool isnum (string s){
-  if(isdigit(s[0])||s[0]=='+'||s[0]=='-'){
-
- for(int i=1;i<s.length();i++){
-    if(!isdigit(s[i]))
-        return false ;
-}}
-else{
-    return false;
+    if(isdigit(s[0])||s[0]=='+'||s[0]=='-'){
+        for(int i=1;i<s.length();i++){
+            if(!isdigit(s[i]))
+                return false;
+        }
+    }
+    
+    else{
+        return false;
+    }
+    
+    return true;
 }
-return true;
-
-
-}
-
-
-
+// great work 😎 9.5/10 ⭐
