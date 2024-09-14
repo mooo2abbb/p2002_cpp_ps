1a30f5a20e9ac606238f47441172d8ff8ab7bc17
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-09-14 13:57:49 +0300

    problem 6 marked

--- cpp/ps0006/ps0006_second_try.cpp
+++ cpp/ps0006/ps0006_second_try.cpp
@@ -1,41 +1,91 @@
 #include<iostream>
 using namespace std;
 
-void counter(double end = 0, double start = 0, int steps = 1) 
+void counter(int end, int start = 0, int steps = 1) 
 {
-    // التحقق من إذا كان الرقم المدخل كسرًا
-    if (start != (int)start) 
+    if (steps == 0 && start == end)
     {
-        cout << "Invalid input.." << endl;
-        return; 
+        cout << end << endl;
+        return;
     }
-    if (steps > 0)
+    
+    if (steps == 0 || start == end)
     {
-      for (int i = start; i <= end; i += steps)
-     {
-        cout << i << "\n";
-        cout << "======================\n";
-     }
+        cout << "Error: can't start from " << start << " to " << end << " with step " << steps << endl;
+        return;
     }
-    else if(steps < 0)
+    
+    if ((end - start) / steps < 0 || (end - start) % steps != 0)
     {
-      for (int i = start; i >= end; i += steps)
-      {
-        cout << i << "\n";
-        cout << "======================\n";
-      }
+        cout << "Error: can't start from " << start << " to " << end << " with step " << steps << endl;
+        return;
     }
-    else 
+    
+    for (start; (end - start) != 0; start += steps)
     {
-      cout<<"wtf how the steps equals 0 ";
+        cout << start << endl;
     }
+    cout << end << endl;
     
 }
 
 int main()
 {
-    cout<<"Please give me the full mark!!! I very tired from this problem\n"; 
-    
-    counter(3,1 );  
+    // running my valid test cases 2/3
+
+    counter(5); // 0 1 2 3 4 5
+    cout << "===============" << endl;
+
+    counter(3, 1); // 1 2 3
+    cout << "===============" << endl;
+
+    counter(-21, 6, -3); // 6 3 0 -3 -6 -9 -12 -15 -18 -21
+    cout << "===============" << endl;
+
+    counter(10, 10, 0); // 10
+    cout << "===============" << endl;
+
+
+
+    // running invalid test cases, have to give clear error 1/4
+
+    counter(100, 10, 0); // Error: can't start from 10 to 100 with step 0
+    cout << "===============" << endl;
+
+    counter(10, 10, 3); // Error: can't start from 10 to 10 with step 3
+    cout << "===============" << endl;
+
+    counter(33, 33, -4); // Error: can't start from 33 to 33 with step -4
+    cout << "===============" << endl;
+
+    counter(-15, 11, 4); // Error: can't start from -15 to 11 with step 4
+    cout << "===============" << endl;
+
+    counter(3, 1, -1); // Error: can't start from 1 to 3 with step -1
+    cout << "===============" << endl;
+
+    counter(-1, 3); // Error: can't start from 3 to -1 with step 1
+    cout << "===============" << endl;
+
+    counter(-1, -8, 3); // Error: can't start from -8 to -1 with step 3
+    cout << "===============" << endl;
+
+    counter(-8, 31, -2); // Error: can't start from 31 to -8 with step -2
+    cout << "===============" << endl;
+
+
+
+    // running bonus test cases +0
+
+    counter(100, 1); // 1 2 3 4 5 6 ... 95 96 97 98 99 100
+    cout << "===============" << endl;
+
+    counter(100); // Massive number of iterations for our humble counter
+    cout << "===============" << endl;
+
+    counter(200, -44); // Massive number of iterations for our humble counter
+
+
+
     return 0;
 }
