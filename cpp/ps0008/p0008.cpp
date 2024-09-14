8a6af640f140e60448a4e11d1d550ca55c0de066
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-09-14 14:44:05 +0300

    problem 8 marked

--- cpp/ps0008/p0008.cpp
+++ cpp/ps0008/p0008.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-#include <limits>
 #include <cctype>
 #include <string>
 using namespace std;
+
+// Decrypting process 3/3
 void decrypt(string msg, int key)
 {
     for (char& c : msg) {
-        if (isupper(c)) {    
+        if (isupper(c)) {
             c = (c - 'A' - key + 26) % 26 + 'A';
         } else if (islower(c)) {
             c = (c - 'a' - key + 26) % 26 + 'a';
@@ -14,18 +15,32 @@ void decrypt(string msg, int key)
     }
     cout << "Decrypted message: " << msg << endl;
 }
+
 int main()
 {
+    // prompting user for valid ciphertext and key
     string ciphertext;  
-    int key;            
+    float key; // to validate floating-point numbers for key
+    
+    // Taking message 1/1
     cout << "Enter the ciphertext: ";
-    getline(cin, ciphertext);//عشان يقرا السطر كامل 
-    cout << "Enter the key: ";
-    while (!(cin >> key)) {
-        cout << "Invalid input. Please enter an integer for the key: ";
-        cin.clear();  //عشان امسح القيم الغير صحيحه
-       cin.ignore(100, '\n'); 
+    getline(cin, ciphertext); //عشان يقرا السطر كامل 
+    
+    // checking key invalidity 2.5/3
+    do
+    {
+        cout << "Enter the key: ";
+        while (!(cin >> key)) {
+            cout << "Invalid input. Please enter an integer for the key: ";
+            cin.clear();  //عشان امسح القيم الغير صحيحه
+            cin.ignore(100, '\n');
+        }
     }
+    while (key != (int)key);
+    key = (int)key % 26; // removing useless multiples
+    
+    // invoke your decrypt function to return the plaintext 1/1
     decrypt(ciphertext, key);
     return 0;
 }
+// great work 😎 9/10 ⭐
